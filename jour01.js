4534a694b1870cb20d7e1be70c4e6377837fcba6
Alin Manole <alin.manole@yahoo.com>
2021-09-27 13:41:10 +0200

    Finish

---
+++ jour01.js
@@ -0,0 +1,50 @@
+// 01 - Hello World 
+
+console.log("Hello World !");
+
+// 02 - String
+
+var test = "My name is Alin";
+console.log(test);
+
+//03 - Concatenation 
+
+var name ="Alin"
+console.log("Nice to meet you " + name );
+
+//04 - String Lenght
+
+var testLenght = "I'm very long !";
+console.log(testLenght.length);
+
+//05 - Replace
+
+var food = "croissant is meh";
+food = food.replace("meh", "so good");
+console.log(food);
+
+//06 - Up and Down 
+
+var basic = "This is Cool";
+var basicUp = basic.toUpperCase();
+var basicDown = basic.toLowerCase();
+console.log(basic , basicUp , basicDown);
+
+//07 - Split
+
+var word ="banana";
+var letters = word.split("");
+console.log(letters)
+
+//08 - Template 
+
+var age = 35;
+var template = console.log(`I'm ${age} years old`);
+console.log(template);
+
+
+// Bonus 
+
+var changed ='"Bonjour"';
+console.log(changed.replace('o', 'a').replace('o', 'a'));
+
